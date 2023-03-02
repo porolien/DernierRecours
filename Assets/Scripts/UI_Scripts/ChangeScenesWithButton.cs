b2672eaef7e4d35a2324eb6c4f0fcb3073ec57fc
NayLiiac <levoyer.kylian24@gmail.com>
2023-03-02 10:16:03 +0100

    Main Menu prototype done

---
+++ Assets/Scripts/UI_Scripts/ChangeScenesWithButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScenesWithButton : MonoBehaviour
+{
+    public string SceneName;
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SceneName);
+
+    }
+}
